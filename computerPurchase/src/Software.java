7c412f7416518a5d543201b669ee7af98d19547b
Francesco Limoni <limoni.francesco21@gmail.com>
2018-10-26 11:31:33 -0600

    Add files via upload

---
+++ computerPurchase/src/Software.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Software extends Computer{
+
+    ArrayList<String> operationSystem = new ArrayList<>();
+
+    public Software(){
+        operationSystem.add("macOS");
+        operationSystem.add("ChromebookOS");
+        operationSystem.add("Windows 7");
+    }
+
+
+}
